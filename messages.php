4b826daf86026b3ab47e13c13c294dd5a3c7246e
Ivan Linan <ilinan87@gmail.com>
2013-04-16 17:59:25 -0700

    added functionality to messages
    Messages inside of database, will populate in the main field of
    messages.php

--- messages.php
+++ messages.php
@@ -4,8 +4,16 @@ session_start();
 if(!isset($_SESSION['currentUser'])){
    header("Location:index.php");
 }
+//connection to the database
+$con=mysqli_connect("ecsmysql","cs431s24","meifithi", "cs431s24");
+
+//grab the current users name
 $currentUser = $_SESSION['currentUser'];
+
+//this variable will be used to decide the CSS inside the footer file
 $_SESSION['NAV'] = 'messages';
+
+//include the header file
 include('header.html');
 ?>
 <!-- content-wrap -->
@@ -13,7 +21,11 @@ include('header.html');
 
     <!-- content -->
     <div id="content" class="clearfix">
-
+<?php
+//query the database to get the items inside mailbox table to populate the users messages
+$sql = "SELECT * FROM mailbox WHERE sender = '$currentUser'";
+$result = mysqli_query($con,$sql);
+?>
    	    <!-- main -->
         <div id="main">
 
@@ -22,11 +34,16 @@ include('header.html');
       	    <h2><a href="messages.php">Messages</a></h2>
 
 					<ul class="archive">
-
+					<?php
+						//populate messages from the database
+						while($row = mysqli_fetch_array($result)) : ?>
 						<li>
-							<div class="post-title"><a href="home.php">Suspendisse bibendum.</a></div>
-							<div class="post-details"><a href="deleteMsg.php">Delete</a> 
+							<div class='post-title'><?php echo $row['subject']; ?></a></div>
+							<div class='post-details'><?php echo$row['msgText']; ?><a href='deleteMsg.php'>Delete</a> 
 						</li>
+				
+						
+					<?php endwhile;?>
 					</ul>					
             </div>
         <!-- /main -->
