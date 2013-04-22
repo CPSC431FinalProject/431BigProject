d96e899acf168186d3ab532df4e3fbb6227304ff
Ivan Linan <ilinan87@gmail.com>
2013-04-22 13:08:51 -0700

    clubs page
    clubs page, with updated headers to correspond

---
+++ clubs.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['currentUser'])){
+   header("Location:index.php");
+}
+$currentUser = $_SESSION['currentUser'];
+
+
+if ($_SESSION['STATUS'] == 'ADMIN'){
+			//since the result is equal to one, we know that the user is an admin
+			//setup to display the admin priviladges page
+			$_SESSION['NAV'] = 'clubs';
+			include_once('adminHeader.html');
+		}
+	else{
+		//grab the session type to know which type of pages the user is able to get
+		$_SESSION['NAV'] = 'clubs';
+		include_once('header.html');
+	}	
+
+//connection to the database
+include_once "mysql.connect.php";
+?>
+<!-- content-wrap -->
+<div id="content-wrap">
+
+    <!-- content -->
+    <div id="content" class="clearfix">
+
+   	    <!-- main -->
+        <div id="main">
+			<div class="main-content">
+
+      	    <h2><a href="forum.php">Clubs</a></h2>					
+            </div>
+
+        <!-- /main -->
+        </div>
+<?php
+include('footer.html');
+?>
