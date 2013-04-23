a405830c0e12a01948f249ce07ce079b33a77e82
Kenneth Ascheri <kascheri12@gmail.com>
2013-04-23 00:06:35 -0700

    fixed connection

--- mysql.connect.php
+++ mysql.connect.php
@@ -1,5 +1,4 @@
 <?php
-//$con = @mysqli_connect("ecsmysql", "cs431s24", "meifithi", "cs431s24");
-$con = mysqli_connect('localhost','kascheri','mysql','BigProject');
+$con = @mysqli_connect("ecsmysql", "cs431s24", "meifithi", "cs431s24");
 if(!$con){ echo mysqli_connect_errno(); }
 ?>
