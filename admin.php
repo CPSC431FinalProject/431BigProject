492078d083570df196f5a60bd0b8378b395c1ced
Kenneth Ascheri <kascheri12@gmail.com>
2013-04-25 18:41:47 -0700

    hooked up admin page

--- admin.php
+++ admin.php
@@ -17,7 +17,78 @@ if($_SESSION['STATUS'] == "ADMIN")
 include_once('adminHeader.html');
 //connection to the database
 include_once "mysql.connect.php";
+
+
+
+/* Code to add a user to the clubMembers table */
+
+if(!empty($_POST['clubname']) && !empty($_POST['info']))
+{
+	//get value of the drop down list
+	$value = $_POST['myDropDown'];			
+
+	
+	if($value == 'newClubAdmin')
+	{ 
+		//add club member to clubMembers table
+		
+		
+		//
+		//
+		//TODO: check to see if there is already a club admin before inserting admin
+		//
+		//
+		$sql = "INSERT INTO clubMembers (clubName,userName,clubAdmin)VALUES('".$_POST['clubname']."','".$_POST['info']."','1')";
+		$result = mysqli_query($con,$sql);
+		
+		if($result)
+		{
+			$return = "<html><body onload=\"alert('Submitted Successfully');\"></body></html>";
+		}
+		else
+		{
+			$return = "<html><body onload=\"alert('Failed to add user to club.');\"></body></html>";
+		}
+	}
+	
+	elseif($value == 'banUser')
+	{
+		//remove club member from clubMembers table
+		$sql = "DELETE FROM clubMembers WHERE userName = '".$_POST['info']."' AND clubName = '".$_POST['clubname']."'";
+		$result = mysqli_query($con,$sql);
+		
+		if($result)
+		{
+			$return = "<html><body onload=\"alert('Member removed from club');\"></body></html>";
+		}
+		else
+		{
+			$return = "<html><body onload=\"alert('Failed to remove user from club.');\"></body></html>";
+		}
+		
+	}
+	
+	elseif($value == 'clubDescription')
+	{
+		//Change club description
+		$sql = "INSERT INTO clubs (clubName,picture,profile) VALUES('".$_POST['clubname']."','/images/".$_POST['clubname'].".jpg','".$_POST['info']."')";
+		$result = mysqli_query($con,$sql);
+		
+		if($result)
+		{
+			$return = "<html><body onload=\"alert('Successfully changed');\"></body></html>";
+		}
+		else
+		{
+			$return = "<html><body onload=\"alert('Failed to change description.');\"></body></html>";
+		}
+	}
+	print $return;
+}
 ?>
+
+
+
 <!-- content-wrap -->
 <div id="content-wrap">
 
@@ -39,30 +110,4 @@ include_once "mysql.connect.php";
         </div>
 <?php
 include_once('footer.html');
-
-/* Code to add a new club*/
-
-if(!empty($_POST['newClub']) && !empty($_POST['clubDescription']))
-	{
-		//grab the new club name
-		$newClub = $_POST['newClub'];
-		
-		$picture = '';
-		
-		//grab the clubs description
-		$description = $_POST['clubDescription'];
-	
-
-		//prepare the mysql query to insert into clubs table
-			$sql="INSERT INTO clubs (clubName, picture, profile)
-			VALUES
-			('$newClub','$picture', '$description')";
-			if(!mysqli_query($con,$sql))
-				{
-					die('Error: ' . mysqli_error($con));
-				}
-				$return = "<html><body onload=\"alert('Submitted');\"><p>New Club Added.</p></body></html>";
-				print($return);
-		//printf("Select returned %d rows.\n", mysqli_num_rows($result));
-	}
 ?>
