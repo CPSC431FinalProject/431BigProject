1d5c736818ad7e88f7ad2207c76a786d055b8818
Kenneth Ascheri <kascheri12@gmail.com>
2013-05-11 15:30:17 -0700

    Revert "fixed status header"
    This reverts commit 014b47e9b66bef74737c0a1da40bcdba594d1ebe.

--- decideStatus.php
+++ decideStatus.php
@@ -8,10 +8,6 @@ if(!isset($_SESSION['STATUS'])){
 	$sql = "SELECT * FROM users WHERE userName = '$currentUser'";
 	$result = mysqli_query($con,$sql);
 	$row = mysqli_fetch_array($result);
-	//see if the user is a club admin
-	$sql2 = "SELECT clubAdmin FROM clubMembers WHERE userName = '$currentUser'";
-	$result2 = mysqli_query($con,$sql2);
-	$row2 = mysqli_fetch_array($result2);
 	if ($row['status'] == "admin"){
 				//since the result is equal to one, we know that the user is an admin
 				//setup to display the admin priviladges page
@@ -19,15 +15,6 @@ if(!isset($_SESSION['STATUS'])){
 				include('adminHeader.html');
 				$_SESSION['NAV'] = 'home';
 			}
-		//if user is clubAdmin
-	else if ($row2['clubAdmin'] == "1"){
-			//since the result is equal to one, we know that the user is an admin
-			//setup to display the club admin priviladges page
-			$_SESSION['STATUS'] = "clubAdmin";
-			include('clubAdminHeader.html');
-			$_SESSION['NAV'] = 'home';
-		}
-		
 	//the user was not a club admin, and not a full admin, so must be a regular user
 	else if ($row['status'] == "USER"){
 			//grab the session type to know which type of pages the user is able to get
@@ -39,7 +26,17 @@ if(!isset($_SESSION['STATUS'])){
 			header('Location:logout.php');
 			}
 			
-
+					//check and see if the user is a club admin	
+	$sql = "SELECT clubAdmin FROM clubMembers WHERE userName = '$currentUser'";
+	$result = mysqli_query($con,$sql);
+	$row = mysqli_fetch_array($result);
+	if ($row['clubAdmin'] == "1"){
+			//since the result is equal to one, we know that the user is an admin
+			//setup to display the club admin priviladges page
+			$_SESSION['STATUS'] = "clubAdmin";
+			include('clubAdminHeader.html');
+			$_SESSION['NAV'] = 'home';
+		}
 }
 else{
 //check and see if the user is a club admin	
@@ -50,14 +47,14 @@ if ($_SESSION['STATUS'] == 'clubAdmin'){
 		include('clubAdminHeader.html');
 	}	
 //check and see if a user is a full admin
-else if ($_SESSION['STATUS'] == 'ADMIN'){
+if ($_SESSION['STATUS'] == 'ADMIN'){
 			//since the result is equal to one, we know that the user is an admin
 			//setup to display the admin priviladges page
 			$_SESSION['STATUS'] = "ADMIN";
 			include('adminHeader.html');
 		}
 //the user was not a club admin, and not a full admin, so must be a regular user
-else if($_SESSION['STATUS'] == 'USER'){
+if($_SESSION['STATUS'] == 'USER'){
 		//grab the session type to know which type of pages the user is able to get
 		$_SESSION['STATUS'] = 'USER';
 		include('header.html');
